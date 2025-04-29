f931a1e70bcc586367952b39547cfd24cef8fb1e
Anika Nawar <nawarani47@gmail.com>
2025-04-29 13:40:40 -0400

    fav button functionality

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,11 +1,17 @@
-// import FavIcon from './FavIcon';
+import FavBadge from './FavBadge';
 import '../styles/PhotoFavButton.scss';
+import { useState } from "react";
 
-const PhotoFavButton = () => {
+
+const PhotoFavButton = (props) => {
+  const [fav, setFav] = useState(true);
+  
+  const handleClick = () => setFav(fav === true ? false : true);
+  
   return (
     <div className="photo-list__fav-icon">
-      <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+      <div className = "photo-list__fav-icon-svg">
+          {fav && < FavBadge onClick = {handleClick} />}
       </div>
     </div>
   );
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,10 +1,11 @@
 import "../styles/PhotoListItem.scss";
-
+import PhotoFavButton from './PhotoFavButton';
 
 const PhotoListItem = (props) => {
   const photo = props.photo;
   return (
     <div className = "photo-list__item">
+      < PhotoFavButton />
       <img src={photo.urls.regular} className = "photo-list__image"/>
       <div className ="photo-list__user-details">
         <img src ={photo.user.profile} className = "photo-list__user-profile"/>
