599273b5bba2d8a1ffa1cb7ec783eafbb846d68b
Anika Nawar <nawarani47@gmail.com>
2025-05-09 18:45:35 -0400

    mock data added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,6 @@
 import HomeRoute from './routes/HomeRoute';
 import './App.scss';
 
-// const sampleDataForPhotoListItem = {
-//   id: 1,
-//   location: {
-//     city: "Montreal",
-//     country: "Canada",
-//   },
-//   urls: {
-//     full: "/Image-1-Full.jpeg",
-//     regular: "/Image-1-Regular.jpeg",
-//   },
-//   user: {
-//     username: "exampleuser",
-//     name: "Joe Example",
-//     profile: "/profile-1.jpg",
-//   },
-// };
-
-// Note: Rendering a single component to build components in isolation
-
 const App = () => {
   return (
     <div className="App">
