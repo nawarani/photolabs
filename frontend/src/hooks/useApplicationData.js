344cde1d87c7957c21c7edd28a8550461e4ef519
Anika Nawar <nawarani47@gmail.com>
2025-05-20 15:47:35 -0400

    useReducer implemented

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,22 +1,51 @@
-import { useState } from "react";
 import { useReducer } from "react";
 
+const ACTIONS = {
+  closeModal: 'closeModal',
+  openModal: 'openModal',
+  checkFav: 'checkFav'
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.closeModal:
+      return {...state, modal:false}
+    case ACTIONS.openModal:
+      return {...state, modal:true, photoModalDetails: action.payload}
+    case ACTIONS.checkFav:
+      let newList = null;
+      if(state.favList.includes(action.payload)) {
+        newList = state.favList.filter((item) => item !== action.payload)
+      } else {
+        newList = [...state.favList, action.payload]
+      }
+      return {...state, favList:newList}
+    default:
+      throw new Error(
+        `Tried to reduce with unsupported action type: ${action.type}`
+      );
+  }
+}
+
+
 const useApplicationData = () => {
+
   const initialState = {
     modal: false,
     photoModalDetails: {},
     favList: []
   }
 
-  const [state, setState] = useState(initialState);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
+  
   const closeModal = () => {
-    setState({...state, modal:false});
+    dispatch({type: ACTIONS.closeModal});
   }
 
   const openModal = (photosDetail) => {
     console.log("inside openmodal", photosDetail);
-    setState({...state, modal:true, photoModalDetails: photosDetail});
+    dispatch({type: ACTIONS.openModal, payload: photosDetail});
   }
 
   const checkFav = (id, favList) => {
@@ -26,7 +55,7 @@ const useApplicationData = () => {
     } else {
       newList = [...favList, id]
     }
-    setState({...state, favList:newList});
+    dispatch({type: ACTIONS.checkFav, payload: id});
   }
 
   return {
@@ -38,42 +67,4 @@ const useApplicationData = () => {
 };
 
 export default useApplicationData;
-
-
-
-// const initialState = {
-//   modal: false,
-//   photoModalDetails: {},
-//   favList: []
-// }
-
-// const [state, dispatch] = useReducer(reducer, initialState);
-
-// export const ACTIONS = {
-//   closeModal: 'closeModal',
-//   openModal: 'openModal',
-//   checkFav: 'checkFav'
-// }
-
-// function reducer(state, action) {
-//   switch (action.type) {
-//     case ACTIONS.closeModal:
-//       return {...state, modal:false}
-//     case ACTIONS.openModal:
-//       return {...state, modal:true, photoModalDetails: photosDetail}
-//     case ACTIONS.checkFav:
-//       let newList = null;
-//       if(state.favList.includes(id)) {
-//         newList = favList.filter((item) => item !== id)
-//       } else {
-//         newList = [...favList, id]
-//       }
-//       return {...state, favList:newList}
-//     default:
-//       throw new Error(
-//         `Tried to reduce with unsupported action type: ${action.type}`
-//       );
-//   }
-// }
-// export default reducer;
 
